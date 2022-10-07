6ac083970271220f1416129ece00776834fb1bfd
feliperez0 <114569545+feliperez0@users.noreply.github.com>
2022-10-07 23:42:16 +0100

    Add files via upload

--- IMC/src/main/java/calcularpeso/IMC.java
+++ IMC/src/main/java/calcularpeso/IMC.java
@@ -1,6 +1,7 @@
 package calcularpeso;
 
 import javafx.application.Application;
+import javafx.beans.binding.DoubleExpression;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleStringProperty;
@@ -9,41 +10,58 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.util.converter.NumberStringConverter;
 
 public class IMC extends Application {
-
-	private TextField texto1 = new TextField();
-	private TextField texto2 = new TextField();;
-	private Label label1 = new Label("IMC: ");
-	private Label label2 = new Label("Bajo peso | Normal | Sobrepeso | Obeso");;
 	
+	private Label imcLabel = new Label();
+	private Label resultLabel = new Label();
+	
+	private Label pesoLabel = new Label("Peso: ");
+	private TextField textPeso = new TextField();
+	private Label kgLabel = new Label("kg");
+	
+	private Label alturaLabel = new Label("Altura: ");
+	private TextField textAltura = new TextField();;
+	private Label cmLabel = new Label("cm");
+
 	private DoubleProperty imc = new SimpleDoubleProperty(0);
 	private DoubleProperty peso = new SimpleDoubleProperty(0);
 	private DoubleProperty altura = new SimpleDoubleProperty(0);
 	private StringProperty result = new SimpleStringProperty();
-
+	private DoubleExpression alturaM;
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {	
 		
-		VBox root =	 new VBox(5, texto1, texto2, label1, label2);
+		HBox pesoPane = new HBox(5);
+		pesoPane.setAlignment(Pos.CENTER);
+		pesoPane.getChildren().addAll(pesoLabel, textPeso, kgLabel);
+		
+		HBox alturaPane = new HBox(5);
+		alturaPane.setAlignment(Pos.CENTER);
+		alturaPane.getChildren().addAll(alturaLabel, textAltura, cmLabel);
+		
+		VBox root =	 new VBox(5);
 		root.setAlignment(Pos.CENTER);
 		root.setFillWidth(false);
+		root.getChildren().addAll(pesoPane, alturaPane, imcLabel, resultLabel);
 		
 		primaryStage.setTitle("IMC");
 		primaryStage.setScene(new Scene(root, 320, 200));
 		primaryStage.show();
 		
-		texto1.textProperty().bindBidirectional(peso, new NumberStringConverter());
-		texto2.textProperty().bindBidirectional(altura, new NumberStringConverter());
-		label1.textProperty().bindBidirectional(imc, new NumberStringConverter());
-		label2.textProperty().bindBidirectional(result);
+		textPeso.textProperty().bindBidirectional(peso, new NumberStringConverter());
+		textAltura.textProperty().bindBidirectional(altura, new NumberStringConverter());
+		imcLabel.textProperty().bind(imc.asString("IMC: %.2f"));
+		resultLabel.textProperty().bindBidirectional(result);
 
 		
-		//altura=altura.divide(100);
-		imc.bind(peso.divide(altura.multiply(altura)));
+		alturaM=altura.divide(100);
+		imc.bind(peso.divide(alturaM.multiply(alturaM)));
 		
 		imc.addListener((o, ov, nv) -> {
 			double i = nv.doubleValue();
